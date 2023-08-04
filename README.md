250ee84792839e6457afa598d9a303e0e2677515
Henry Smith <artificiallyinteresting@gmail.com>
2023-08-04 13:33:45 -0700

    Chatbot progress

--- README.md
+++ README.md
@@ -6,6 +6,6 @@
 
 2. Get an API key from here: https://platform.openai.com/account/api-keys
 
-3. Put your API key in a file called openai_api_key.txt (and don't upload this to github)
+3. Put your API key in a file called .env (and don't upload this to github)
 
 4. Place docs in docs folder
--- main.py
+++ main.py
@@ -7,10 +7,11 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chains.question_answering import load_qa_chain
-
-# Load api key
-f = open("openai_api_key.txt", "r")
-openai_api_key = f.read()
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
 
 # Load documents
 loader = PyPDFLoader("docs/output.pdf")
@@ -19,10 +20,11 @@ pages = loader.load_and_split()
 runSemanticSearch = False
 runChatbot = False
 runChain = False
+runQAVectordb = True
 
 if (runSemanticSearch):
     # Testing semantic search
-    faiss_index = FAISS.from_documents(pages, OpenAIEmbeddings(openai_api_key=openai_api_key))
+    faiss_index = FAISS.from_documents(pages, OpenAIEmbeddings())
     docs = faiss_index.similarity_search("helper clerk displacement", k=4)
     for doc in docs:
         print(str(doc.metadata["page"]) + ":", doc.page_content[:300])
@@ -30,7 +32,7 @@ if (runSemanticSearch):
 
 if (runChatbot):
     # Testing chatbot (currently does not use the documents)
-    chat = ChatOpenAI(temperature=0, openai_api_key=openai_api_key)
+    chat = ChatOpenAI(model_name="gpt-3.5-turbo")
     out = chat(
         [
             SystemMessage(content="You are a union expert. You are going to answer questions about union contracts."),
@@ -41,8 +43,29 @@ if (runChatbot):
 
 if (runChain):
     # Create a "refine" chain using langchain
-    chat = ChatOpenAI(temperature=0, openai_api_key=openai_api_key)
+    chat = ChatOpenAI(temperature=0)
     chain = load_qa_chain(chat, chain_type="refine")
-    query = "When can Helper Clerks be placed in the Apprentice Grocery Clerk classification."
+    query = "When can Helper Clerks be placed in the Apprentice Grocery Clerk classification?"
     chain({"input_documents": pages, "question": query}, return_only_outputs=True)
 
+if (runQAVectordb):
+    #See https://python.langchain.com/docs/use_cases/question_answering/
+
+    # Split the Document into chunks for embedding and vector storage
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 0)
+    all_splits = text_splitter.split_documents(pages)
+
+    # Create the vector store
+    vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbeddings())
+
+    # Search the vector store for a phrase
+    searchPhrase = "Apprentice Grocery Clerk classification"
+    returnedDocs = vectorstore.similarity_search(searchPhrase)
+    print(returnedDocs)
+    
+    # Direct question answering system
+    question = "When can Helper Clerks be placed in the Apprentice Grocery Clerk classification?"
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    qa_chain = RetrievalQA.from_chain_type(llm,retriever=vectorstore.as_retriever())
+    answer = qa_chain({"query": question})
+    print(answer)
